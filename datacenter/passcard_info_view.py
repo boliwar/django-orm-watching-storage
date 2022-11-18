67dd371d4035cf23331fe1af77237c7701c44eb2
boliwar <boliwar@gmail.com>
2022-11-18 13:17:51 +0200

    ловим неправильный номер пропуска

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,13 +2,13 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from datacenter.models import get_duration, format_duration, get_strdate_timezone, is_visit_long
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode= passcode)[0]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     # Программируем здесь
-
-    passcard_visits = Visit.objects.filter(passcard= passcard )
+    passcard_visits = Visit.objects.filter(passcard=passcard )
 
     this_passcard_visits = []
     for visit in passcard_visits:
