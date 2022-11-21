d421b889ddae964eb400d07179d4ffc0f3204a79
boliwar <boliwar@gmail.com>
2022-11-21 19:26:14 +0200

    настройка ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,5 @@
 import os
 from environs import Env
-import dj_database_url
 
 env = Env()
 env.read_env()
@@ -24,7 +23,7 @@ DEBUG = env.bool("DEBUG", False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
